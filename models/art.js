bb46b7837d67758dd3790ccc20900c90b47402f7
Rangga Eki Syahrial <syahrialrangga11@gmail.com>
2021-04-17 08:08:00 +0700

    Revise User model's attributes

--- models/art.js
+++ models/art.js
@@ -1,6 +1,10 @@
 const { Schema, model, ObjectId } = require("mongoose");
 
 const artSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   image_url: {
     type: String,
     required: true,
@@ -9,16 +13,14 @@ const artSchema = Schema({
     type: Number,
     required: true,
   },
-  like: [
+  user: {
+    type: ObjectId,
+    ref: "User",
+  },
+  likes: [
     {
       type: ObjectId,
       ref: "User",
-    },
-  ],
-  categories: [
-    {
-      type: ObjectId,
-      ref: "Category",
     },
   ],
 });
