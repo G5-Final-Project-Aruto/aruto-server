94a050e823e0149bf62866b98805532d1728b71d
Rangga Eki Syahrial <syahrialrangga11@gmail.com>
2021-04-18 04:32:44 +0700

    Testing getAll and getOne art

--- controllers/artController.js
+++ controllers/artController.js
@@ -20,9 +20,10 @@ class Controller {
       next(error);
     }
   }
+
   static async getAllArt(req, res, next) {
     try {
-      const arts = await Art.find({}).populate("user");
+      const arts = await Art.find().populate("user");
       const dataArts = arts.map((art) => {
         return {
           _id: art._id,
@@ -34,6 +35,7 @@ class Controller {
           image_url: art.image_url,
           price: art.price,
           likes: art.likes.length,
+          categories: art.categories,
         };
       });
       res.status(200).json(dataArts);
@@ -41,16 +43,29 @@ class Controller {
       next(error);
     }
   }
+
   static async getOneArt(req, res, next) {
     try {
       const art = await Art.findOne({
         _id: req.params.id,
+      }).populate("user");
+      res.status(200).json({
+        _id: art._id,
+        user: {
+          _id: art.user._id,
+          username: art.user.username,
+        },
+        title: art.title,
+        image_url: art.image_url,
+        price: art.price,
+        likes: art.likes.length,
+        categories: art.categories,
       });
-      res.status(200).json(art);
     } catch (error) {
       next(error);
     }
   }
+
   static async updateArt(req, res, next) {
     try {
       const { title, image_url, price } = req.body;
@@ -88,6 +103,7 @@ class Controller {
       next(error);
     }
   }
+
   static async addLikeArt(req, res, next) {
     try {
       const art = await Art.updateOne(
