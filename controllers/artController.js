9033145fa34001c7a5396699b61915a7d26fc6d0
Rangga Eki Syahrial <syahrialrangga11@gmail.com>
2021-04-18 04:02:43 +0700

    Updated item: 1. Testing get all catagories 2. Testing create arts

--- controllers/artController.js
+++ controllers/artController.js
@@ -1,112 +1,110 @@
-const { Art, User } = require("../models");
-const { hashPassword, verifyPassword, getToken } = require("../helpers");
-const { find } = require("../models/art");
+const { Art } = require("../models");
+const { uploadImage, deleteImage } = require("../helpers");
 
 class Controller {
-  static async createArt( req, res, next ) {
+  static async createArt(req, res, next) {
+    let image_url = "";
     try {
-      const { title, image_url, price } = req.body
-      const newArt = {
-        title,
-        image_url,
-        price,
-        user: req.currentUser._id
-      }
+      const data = await uploadImage(req);
+      image_url = data.image_url;
 
-      const art = await Art.create(newArt)
-      await User.updateOne({
-        _id: req.currentUser._id
-      }, {
-        $push: {
-          arts: art
-        }
-      })
-      res.status(201).json(art)
+      const art = await Art.create({
+        ...data,
+        categories: data.categories.split(",").map((cat) => cat.trim()),
+        user: req.currentUser._id,
+      });
+
+      res.status(201).json(art);
     } catch (error) {
-      next(error)
+      deleteImage(image_url);
+      next(error);
     }
   }
-  static async getAllArt( req, res, next ) {
+  static async getAllArt(req, res, next) {
     try {
-      const arts = await Art.find({}).populate('user')
-      const dataArts = arts.map(art => {
+      const arts = await Art.find({}).populate("user");
+      const dataArts = arts.map((art) => {
         return {
           _id: art._id,
           user: {
             _id: art.user._id,
-            username: art.user.username
+            username: art.user.username,
           },
           title: art.title,
           image_url: art.image_url,
           price: art.price,
-          likes: art.likes.length
-        }
-      })
-      res.status(200).json(dataArts)
+          likes: art.likes.length,
+        };
+      });
+      res.status(200).json(dataArts);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
-  static async getOneArt( req, res, next) {
+  static async getOneArt(req, res, next) {
     try {
       const art = await Art.findOne({
-        _id: req.params.id
-      })
-      res.status(200).json(art)
+        _id: req.params.id,
+      });
+      res.status(200).json(art);
     } catch (error) {
-      next(error)
-    }    
+      next(error);
+    }
   }
-  static async updateArt( req, res, next) {
+  static async updateArt(req, res, next) {
     try {
-      const { title,image_url, price } = req.body
-      console.log(req.params.id)
+      const { title, image_url, price } = req.body;
       const dataArt = await Art.findOne({
-        _id: req.params.id
-      })
-      console.log(dataArt)
-      const art = await Art.updateOne({
-        _id: req.params.id
-      },{
-        $set: {
-          title,
-          image_url,
-          price
+        _id: req.params.id,
+      });
+      const art = await Art.updateOne(
+        {
+          _id: req.params.id,
         },
-      })
-      res.status(200).json(dataArt)
+        {
+          $set: {
+            title,
+            image_url,
+            price,
+          },
+        }
+      );
+      res.status(200).json(dataArt);
     } catch (error) {
-      next(error)
-    }    
+      next(error);
+    }
   }
-  static async deleteArt( req, res, next) {
+  static async deleteArt(req, res, next) {
     try {
       const art = await Art.deleteOne({
-        _id: req.params.id
-      })
-      if(art.deletedCount > 0) {
-        res.status(200).json({message: "success delete art"})
-      }else{
-        throw ({name: "Art not found"})
+        _id: req.params.id,
+      });
+      if (art.deletedCount > 0) {
+        res.status(200).json({ message: "success delete art" });
+      } else {
+        throw { name: "Art not found" };
       }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
   static async addLikeArt(req, res, next) {
     try {
-      const art = await Art.updateOne({
-        _id: req.params.id
-      },{
-        $push:{
-          likes: req.currentUser
+      const art = await Art.updateOne(
+        {
+          _id: req.params.id,
+        },
+        {
+          $push: {
+            likes: req.currentUser,
+          },
         }
-      })
+      );
       res.status(200).json({
-        message: "Art has been liked"
-      })
+        message: "Art has been liked",
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
